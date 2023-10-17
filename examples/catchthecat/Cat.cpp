d3400734a6b86ca3d2d9a785f21158fbd79ee946
galuxes <90073330+galuxes@users.noreply.github.com>
2023-10-17 02:41:24 -0400

    Cat that doesnt work

--- examples/catchthecat/Cat.cpp
+++ examples/catchthecat/Cat.cpp
@@ -2,23 +2,111 @@
 #include "World.h"
 #include <stdexcept>
 
-Point2D Cat::Move(World* world) {
-  auto rand = Random::Range(0, 5);
-  auto pos = world->getCat();
-  switch (rand) {
-    case 0:
-      return World::NE(pos);
-    case 1:
-      return World::NW(pos);
-    case 2:
-      return World::E(pos);
-    case 3:
-      return World::W(pos);
-    case 4:
-      return World::SW(pos);
-    case 5:
-      return World::SE(pos);
-    default:
-      throw "random out of range";
+Point2D Cat::Move(World* world)
+{
+  int numSteps = 0;
+  bool exitFound = false;
+
+  frontier.push_back(world->getCat());
+
+  while (!exitFound)
+  {
+    for (int i = 0; i < frontier.size(); i++)
+    {
+      vector<Point2D> neighbors = GetNeighbors(frontier[i], world);
+
+      for (int j = 0; j < neighbors.size(); j++)
+      {
+        nextFrontier.push_back(neighbors[j]);
+      }
+
+      tilesInRadius.push_back(frontier[i]);
+
+      if(world->catWinsOnSpace(neighbors.back()))
+      {
+        exitFound = true;
+        break;
+      }
+    }
+    numSteps++;
+    frontier = nextFrontier;
+    nextFrontier.clear();
   }
+
+  vector<Point2D> open, closed;
+
+  return exit;
+
 }
+
+vector<Point2D> Cat::GetNeighbors(Point2D point, World* world)//returns true if found exit
+{
+  vector<Point2D> neighbors;
+  Point2D tile;
+  for (int i = 0; i < 6; i++)
+  {
+    bool tileValid = true;
+    switch (i)
+    {
+      case 0:
+        tile = world->E(point);
+        break;
+      case 1:
+        tile = world->NE(point);
+        break;
+      case 2:
+        tile = world->NW(point);
+        break;
+      case 3:
+        tile = world->W(point);
+        break;
+      case 4:
+        tile = world->SW(point);
+        break;
+      case 5:
+        tile = world->SE(point);
+        break;
+      default:
+        break;
+    }
+
+    if (!world->isValidPosition(tile))//checks in bounds
+    {
+      tileValid = false;
+    }
+
+    if(world->getContent(tile))//checks if blocked
+    {
+      tileValid = false;
+    }
+
+    for (int j = 0; j < neighbors.size(); j++)//checks not in list already
+    {
+      if(neighbors[i] == tile)
+      {
+        tileValid = false;
+      }
+    }
+
+    for (int j = 0; j < nextFrontier.size(); j++)//checks not in list already
+    {
+      if(nextFrontier[i] == tile)
+      {
+        tileValid = false;
+      }
+    }
+
+    if(tileValid)//adds tile if valid is true
+    {
+      neighbors.push_back(tile);
+    }
+
+    if (world->catWinsOnSpace(tile))
+    {
+      exit = tile;
+      return neighbors;
+    }
+  }
+  return neighbors;
+}
+
--- examples/catchthecat/Cat.h
+++ examples/catchthecat/Cat.h
@@ -1,12 +1,19 @@
 #ifndef CAT_H
 #define CAT_H
 
+#include <vector>
 #include "Agent.h"
+using namespace std;
 
 class Cat : public Agent {
 public:
   explicit Cat() : Agent(){};
   Point2D Move(World*) override;
+private:
+  vector<Point2D> GetNeighbors(Point2D point,World* world);
+
+  vector<Point2D> tilesInRadius, nextFrontier, frontier;
+  Point2D exit;
 };
 
 #endif  // CAT_H
